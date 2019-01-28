d3bf9afc8637c2c33f18553f1ff9f66a48ae7724
Josiahngugi <mainajosiahdev@gmail.com>
2019-01-28 04:24:26 -0800

    import isPrime function

---
+++ src/main/java/Memorize.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Memorize {
+
+ public void memorize() {
+  Scanner input =new Scanner(System.in);
+  System.out.println("enter number");
+  int k= input.nextInt();
+
+  public boolean isPrime(int n)
+  {
+   // Corner case
+   if (n <= 1)
+    return false;
+
+   // Check from 2 to n-1
+   for (int i = 2; i < n; i++)
+    if (n % i == 0)
+     return false;
+
+   return true;
+  }
+}
